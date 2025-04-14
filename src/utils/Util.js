6a4f10e3f8a02f937b8f660253b214d470048cd5
SaidSuyv <sirsuyvi@gmail.com>
2025-04-13 21:53:19 -0500

    fix utils for pacientes y citas tables

--- src/utils/Util.js
+++ src/utils/Util.js
@@ -1,6 +1,10 @@
 
 export function formatDate(dateStr) {
-    const date = new Date(dateStr + 'T00:00:00');
+    var date = null
+    if (typeof dateStr == 'object')
+        date = dateStr
+    else
+        date = new Date(dateStr + 'T00:00:00');
     const day = String(date.getDate()).padStart(2, '0');
     const month = String(date.getMonth() + 1).padStart(2, '0');
     const year = date.getFullYear();
