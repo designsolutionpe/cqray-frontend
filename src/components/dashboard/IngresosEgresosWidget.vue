7fd2b57c7fe8c107cfa1cc324b4bce26a2ec3e0b
SaidSuyv <sirsuyvi@gmail.com>
2025-06-19 22:42:57 -0500

    update: stats cleaning

--- src/components/dashboard/IngresosEgresosWidget.vue
+++ src/components/dashboard/IngresosEgresosWidget.vue
@@ -7,24 +7,29 @@ const { getPrimary, getSurface, isDarkTheme } = useLayout();
 const chartData = ref(null);
 const chartOptions = ref(null);
 
+const labels = ref([])
+const ingresos = ref([])
+const egresos = ref([])
+const totales = ref([])
+
 function setChartData() {
     const documentStyle = getComputedStyle(document.documentElement);
 
     return {
-        labels: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'], // Meses
+        labels: labels.value, // Meses
         datasets: [
             {
                 type: 'bar',
                 label: 'Ingresos',
                 backgroundColor: documentStyle.getPropertyValue('--secondary-color'), // Color para ingresos
-                data: [10000, 12000, 13000, 15000, 12000, 14000, 13000, 16000, 11000, 15000, 17000, 19000], // Datos de ingresos
+                data: ingresos.value, // Datos de ingresos
                 barThickness: 32
             },
             {
                 type: 'bar',
                 label: 'Egresos',
                 backgroundColor: documentStyle.getPropertyValue('--primary-color'), // Color para egresos
-                data: [-5000, -6000, -7000, -8000, -7500, -6500, -6000, -8500, -7000, -8000, -9000, -9500], // Datos de egresos (valores negativos)
+                data: egresos.value, // Datos de egresos (valores negativos)
                 barThickness: 32
             }
         ]
--- src/components/gestion/pacientes/directorio/DatosPersonalesTab.vue
+++ src/components/gestion/pacientes/directorio/DatosPersonalesTab.vue
@@ -12,23 +12,23 @@ const props = defineProps({
     <div id="personal-informacion">
       <p class="text-xl font-bold text-secondary">Información personal</p>
       <div class="grid grid-cols-3 gap-5">
-        <div class="flex flex-col">
+        <div class="col-span-3 md:col-span-1 flex flex-col">
           <p class="text-gray-400 m-0">Tipo documento</p>
           <p class="">{{ props.oPaciente.persona.tipo_documento || '---' }}</p>
         </div>
-        <div class="flex flex-col">
+        <div class="col-span-3 md:col-span-1 flex flex-col">
           <p class="text-gray-400 m-0">Numero documento</p>
           <p class="">{{ props.oPaciente.persona.numero_documento || '---' }}</p>
         </div>
-        <div class="flex flex-col">
+        <div class="col-span-3 md:col-span-1 flex flex-col">
           <p class="text-gray-400 m-0">Nombre completo</p>
           <p class="">{{ props.oPaciente.persona.nombre || '---' }} {{ props.oPaciente.persona.apellido || '---' }}</p>
         </div>
-        <div class="flex flex-col">
+        <div class="col-span-3 md:col-span-1 flex flex-col">
           <p class="text-gray-400 m-0">Género</p>
           <p class="">{{ props.oPaciente.persona.genero || '---' }}</p>
         </div>
-        <div class="flex flex-col">
+        <div class="col-span-3 md:col-span-1 flex flex-col">
           <p class="text-gray-400 m-0">Fecha de nacimiento</p>
           <p class="">{{ props.oPaciente.persona.fecha_nacimiento }}</p>
         </div>
@@ -38,11 +38,11 @@ const props = defineProps({
     <div id="paciente-informacion">
       <p class="text-xl font-bold text-secondary">Información de paciente</p>
       <div class="grid grid-cols-3 gap-5">
-        <div class="flex flex-col">
+        <div class="col-span-3 md:col-span-1 flex flex-col">
           <p class="text-gray-400 m-0">Sede</p>
           <p>{{ props.oPaciente.sede.nombre || '---' }}</p>
         </div>
-        <div class="flex flex-col">
+        <div class="col-span-3 md:col-span-1 flex flex-col">
           <p class="text-gray-400 m-0">Estado</p>
           <p>{{ props.oPaciente.estado.nombre || '---' }}</p>
         </div>
@@ -52,13 +52,17 @@ const props = defineProps({
     <div id="servicio-informacion">
       <p class="text-xl font-bold text-secondary">Información de servicio</p>
       <div class="grid grid-cols-3 gap-5">
-        <div class="flex flex-col">
+        <div class="col-span-3 md:col-span-1 flex flex-col">
           <p class="text-gray-400 m-0">Paquete</p>
-          <p>{{ props.oPaciente.paquete_activo.paquete.nombre || '---' }}</p>
+          <p>{{ props.oPaciente.paquete_activo.paquete ? props.oPaciente.paquete_activo.paquete.nombre : '---' }}</p>
         </div>
-        <div class="flex flex-col">
+        <div class="col-span-3 md:col-span-1 flex flex-col">
           <p class="text-gray-400 m-0">Precio total</p>
-          <p>S/{{ props.oPaciente.paquete_activo.paquete.precio_venta || '---' }}</p>
+          <p>S/{{ props.oPaciente.paquete_activo.paquete ? props.oPaciente.paquete_activo.paquete.precio_venta : '---' }}</p>
+        </div>
+        <div class="col-span-3 md:col-span-1 flex flex-col">
+            <p class="text-gray-400 m-0">Deuda pendiente</p>
+            <p>S/{{ props.oPaciente.deuda ? (parseFloat(props.oPaciente.deuda) / 100).toFixed(2) : "---" }}</p>
         </div>
       </div>
     </div>
