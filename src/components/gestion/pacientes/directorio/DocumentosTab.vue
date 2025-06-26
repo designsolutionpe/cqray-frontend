0077eb4c965576e9ef3a47f88e267873d04f6864
SaidSuyv <sirsuyvi@gmail.com>
2025-06-25 22:13:29 -0500

    update directorio

---
+++ src/components/gestion/pacientes/directorio/DocumentosTab.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+    const props = defineProps({
+        aDocumentos: {
+            type: Array,
+            required: true
+        }
+    })
+
+</script>
+<template>
+    <!-- SI HAY DOCUMENTOS -->
+    <div class=""></div>
+    <!-- SI NO HAY DOCUMENTOS -->
+    <div></div>
+</template>
